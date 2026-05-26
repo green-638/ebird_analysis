d87e56e4bd361f80ac6c8aeb19d2589e7aa3aa25
Connor Hall <connor582h@gmail.com>
2026-05-26 13:23:37 -0400

    change: partial CSV updates when invalid region code encountered. updated readme with script instructions

--- load_data.py
+++ load_data.py
@@ -33,28 +33,8 @@ days = int(input('# of days back to pull data from (<=30): '))
 if days > 30:
   print('Reports cannot be over 30 days old')
   sys.exit()
+  
 
-
-all_responses = []
-# api call
-for loc in locations:
-  url = f'https://api.ebird.org/v2/data/obs/{loc}/recent/notable?back={days}&detail=full'
-  headers = {
-    'X-eBirdApiToken': token
-  }
-  
-  response = requests.request("GET", url, headers=headers, data=payload)
-  # get request status code
-  status_code = response.status_code
-  # exit program if invalid
-  if status_code != 200:
-    print(f'"{loc}" is an invalid region code')
-    sys.exit()
-    
-  resp_json = json.loads(response.text)
-  all_responses += resp_json
-  
-  
 # initialize csv if empty or doesn't exist
 path = input('CSV path: ')
 # if file exists
@@ -64,36 +44,63 @@ if os.path.isfile(path):
         create_csv(path)
 else:
     create_csv(path)
-  
-
-df = pd.read_csv(path)
-# iterate through each location's reports
-for report in all_responses:
-  keys = report.keys()
-  # add 'X' to reports with no species quantity
-  if 'howMany' not in keys:
-    report['howMany'] = 'X'
-  # mark subnational1 and subnational2 as N/A when necessary
-  if 'subnational1Name' not in keys:
-    report['subnational1Name'] = 'N/A'
-  if 'subnational2Name' not in keys:
-    report['subnational2Name'] = 'N/A'
     
-  # create new row
-  row = pd.DataFrame({'checklist_id': [report['subId']],
-                      'species_code': [report['speciesCode']],
-                      'species_name': [report['comName']],
-                      'loc_id': [report['locId']],
-                      'loc_name': [report['locName']],
-                      'date': [report['obsDt']],
-                      'quantity': [report['howMany']],
-                      'country': [report['countryName']],
-                      'subnational1': [report['subnational1Name']],
-                      'subnational2': [report['subnational2Name']],
-                      'first_name': [report['firstName']],
-                      'last_name': [report['lastName']]})
+df = pd.read_csv(path)
+
+
+# api call
+loc_num = 0
+for loc in locations:
+  url = f'https://api.ebird.org/v2/data/obs/{loc}/recent/notable?back={days}&detail=full'
+  headers = {
+    'X-eBirdApiToken': token
+  }
   
-  df = pd.concat([df, row], ignore_index=True)    
+  response = requests.request("GET", url, headers=headers, data=payload)
+  
+  # get request status code
+  status_code = response.status_code
+  # exit program if invalid
+  if status_code != 200:
+    print(f'"{loc}" is an invalid region code')
+    # update CSV if changes were made
+    if loc_num > 0:
+      df = df.drop_duplicates(keep='last', ignore_index=True)
+      df.to_csv(path)
+      print(f'Partial data load complete. Following regions loaded: {locations[:loc_num]}')
+      
+    sys.exit()
+    
+  resp_json = json.loads(response.text)
+  
+  for report in resp_json:
+    keys = report.keys()
+    # add 'X' to reports with no species quantity
+    if 'howMany' not in keys:
+      report['howMany'] = 'X'
+    # mark subnational1 and subnational2 as N/A when necessary
+    if 'subnational1Name' not in keys:
+      report['subnational1Name'] = 'N/A'
+    if 'subnational2Name' not in keys:
+      report['subnational2Name'] = 'N/A'
+      
+    # create new row
+    row = pd.DataFrame({'checklist_id': [report['subId']],
+                        'species_code': [report['speciesCode']],
+                        'species_name': [report['comName']],
+                        'loc_id': [report['locId']],
+                        'loc_name': [report['locName']],
+                        'date': [report['obsDt']],
+                        'quantity': [report['howMany']],
+                        'country': [report['countryName']],
+                        'subnational1': [report['subnational1Name']],
+                        'subnational2': [report['subnational2Name']],
+                        'first_name': [report['firstName']],
+                        'last_name': [report['lastName']]})
+    
+    df = pd.concat([df, row], ignore_index=True)    
+  
+  loc_num += 1
     
 df = df.drop_duplicates(keep='last', ignore_index=True)
 df.to_csv(path)
